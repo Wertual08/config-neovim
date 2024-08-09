e714b26d1a59fc9ef2c0dcbe1c72f1a557c3b47f
Wertual <wertual08@gmail.com>
2024-08-09 13:29:44 +0300

    Add far.vim

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -1,5 +1,8 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    dependencies = "nvim-lua/plenary.nvim",
-    opts = require("config.telescope"),
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = "nvim-lua/plenary.nvim",
+        opts = require("config.telescope"),
+    },
+    {  'brooth/far.vim' },
 }
